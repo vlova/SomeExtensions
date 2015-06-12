739c16e38a394a75d5e6dc33de507a5df39474c0
Viktor Lova <nsinreal@gmail.com>
2015-06-12 05:02:16 +0300

    replace spaces to tabs

--- SomeExtensions/Refactorings/ReorderMethod/ReorderBlockProvider.cs
+++ SomeExtensions/Refactorings/ReorderMethod/ReorderBlockProvider.cs
@@ -8,43 +8,43 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace SomeExtensions.Refactorings.ReorderBlock {
-    [ExportCodeRefactoringProvider(nameof(ReorderBlockProvider), LanguageNames.CSharp), Shared]
-    internal class ReorderBlockProvider : BaseRefactoringProvider<BlockSyntax> {
-        protected override int? FindUpLimit => 5;
+	[ExportCodeRefactoringProvider(nameof(ReorderBlockProvider), LanguageNames.CSharp), Shared]
+	internal class ReorderBlockProvider : BaseRefactoringProvider<BlockSyntax> {
+		protected override int? FindUpLimit => 5;
 
-        protected override bool IsGood(BlockSyntax block) {
-            var locals = block.Statements.OfType<LocalDeclarationStatementSyntax>();
-            var localsAreGood = locals.All(l => l?.Declaration?.Variables.IsSingle() ?? false);
-            return locals.Any() && localsAreGood && IfsAreGood(block);
-        }
+		protected override bool IsGood(BlockSyntax block) {
+			var locals = block.Statements.OfType<LocalDeclarationStatementSyntax>();
+			var localsAreGood = locals.All(l => l?.Declaration?.Variables.IsSingle() ?? false);
+			return locals.Any() && localsAreGood && IfsAreGood(block);
+		}
 
-        private bool IfsAreGood(BlockSyntax block) {
-            var breakingIfs = block
-                .DescendantNodes()
-                .OfType<IfStatementSyntax>()
-                .Where(f => f.DescendantNodes().Any(NodeExtensions.IsBreakable));
+		private bool IfsAreGood(BlockSyntax block) {
+			var breakingIfs = block
+				.DescendantNodes()
+				.OfType<IfStatementSyntax>()
+				.Where(f => f.DescendantNodes().Any(NodeExtensions.IsBreakable));
 
-            return breakingIfs.All(@if => IsLastIf(block, @if));
-        }
+			return breakingIfs.All(@if => IsLastIf(block, @if));
+		}
 
-        private bool IsLastIf(BlockSyntax block, IfStatementSyntax @if) {
-            SyntaxNode node = @if;
-            while (node != block) {
-                var parent = node.Parent;
-                if (parent is BlockSyntax) {
-                    var parentBlock = parent as BlockSyntax;
-                    var isLast = (parentBlock.Statements.Last() == node);
-                    if (!isLast) return false;
-                }
+		private bool IsLastIf(BlockSyntax block, IfStatementSyntax @if) {
+			SyntaxNode node = @if;
+			while (node != block) {
+				var parent = node.Parent;
+				if (parent is BlockSyntax) {
+					var parentBlock = parent as BlockSyntax;
+					var isLast = (parentBlock.Statements.Last() == node);
+					if (!isLast) return false;
+				}
 
-                node = parent;
-            }
+				node = parent;
+			}
 
-            return true;
-        }
+			return true;
+		}
 
-        protected override async Task ComputeRefactoringsAsync(RefactoringContext context, BlockSyntax block) {
-            context.RegisterAsync(new ReorderBlockRefactoring(block, await context.SemanticModelAsync));
-        }
-    }
+		protected override async Task ComputeRefactoringsAsync(RefactoringContext context, BlockSyntax block) {
+			context.RegisterAsync(new ReorderBlockRefactoring(block, await context.SemanticModelAsync));
+		}
+	}
 }
